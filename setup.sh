8888c28bca934bd69219f404dca7df9c6f531919
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-06-25 22:23:20 +0100

    add logs to setup.sh

--- setup.sh
+++ setup.sh
@@ -48,19 +48,26 @@ setup_extra "alacritty" # required by i3
 setup_extra "i3"
 
 # Essential utilities
+log "Installing essential utilities"
 pacman -S --noconfirm eza plocate
 
 # VirtualBox Guest Additions
+log "Installing VirtualBox Guest Additions"
 pacman -S --noconfirm virtualbox-guest-utils
 systemctl enable --now vboxservice
 
 # VS Code
+log "Installing Visual Studio Code"
 yay -S --noconfirm visual-studio-code-bin
 
 # Apps/Media
+log "Installing apps and media"
 pacman -S --noconfirm vivaldi vivaldi-ffmpeg-codecs firefox vlc spotify-launcher
 
 # Set /home/$username permissions
+log "Setting /home/$username permissions"
 chown -R $username:$username /home/$username
 
-#reboot
+log "Setup complete. Rebooting in 5 seconds..."
+sleep 5
+reboot
