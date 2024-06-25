a6fbcfac77ff78d5203c3c0f8f429143b0e1c92d
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-06-25 20:48:49 +0100

    Refactor setup script and improve network setup

--- setup.sh
+++ setup.sh
@@ -3,12 +3,6 @@
 ### Enable NetworkManager
 systemctl enable --now NetworkManager
 
-# Load utils
-source <(curl -fsSL https://raw.githubusercontent.com/itsbekas/arch-install/master/utils.sh)
-
-setup_extra "reflector"
-setup_extra "pacman"
-
 ### Setup user
 read -p "Enter the username: " username
 valid_password=false
@@ -29,6 +23,19 @@ echo "$username:$password" | chpasswd
 
 # Allow wheel group to use sudo
 sed -i 's/^# \(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers
+
+# Wait for the network to be up
+while ! ping -c 1 archlinux.org &> /dev/null; do
+    echo "Waiting for network..."
+    sleep 1
+done
+
+# Load utils
+source <(curl -fsSL https://raw.githubusercontent.com/itsbekas/arch-install/master/utils.sh)
+
+setup_extra "reflector"
+setup_extra "pacman"
+
 
 setup_extra "yay"
 setup_extra "zsh"
