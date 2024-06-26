fc845274ed60e5b361c3e014994e106930b33eef
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2024-06-26 13:10:35 +0100

    Refactor VS Code configuration and enable auto-formatting on save for Python files

---
+++ update_configs.sh
@@ -0,0 +1,12 @@
+# Download utils
+curl -fsSL https://raw.githubusercontent.com/itsbekas/arch-install/master/utils.sh -o /tmp/arch-install/utils.sh
+source /tmp/arch-install/utils.sh
+
+# Download the config files
+download_config "i3/config" "/home/$username/.config/i3/config"
+download_config "reflector/reflector.conf" "/etc/xdg/reflector/reflector.conf"
+download_config "vscode/settings.json" "/home/$username/.config/Code/User/settings.json"
+download_config "vscode/keybindings.json" "/home/$username/.config/Code/User/keybindings.json"
+download_config "xorg-xinit/.xinitrc" "/home/$username/.xinitrc"
+download_config "zsh/.zshrc" "/home/$username/.zshrc"
+download_config "zsh/.p10k.zsh" "/home/$username/.p10k.zsh"
